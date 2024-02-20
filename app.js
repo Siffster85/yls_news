346e73fcf0baf39326c5eb70a9537b92f158da6c
Siffster85 <ylsaxon@googlemail.com>
2024-02-20 10:26:30 +0000

    typos corrected

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const { handlePSQLErrors, handleCustomErrors } = require('./Errors/errorhandler')
 
-const { getTopics, getEndpoints, getArticle } = require('./Controller/topics.controller')
+const { getTopics, getEndpoints } = require('./Controller/topics.controller')
+
+const { getArticle } = require('./Controller/articles.controller')
 
 //app.use(express.json()) will go here
 
@@ -14,15 +16,12 @@ app.get('/api/articles/:article_id', getArticle)
 
 
 
-
-//catch all for 404
 app.all ('/*', (req, res) => {
     res.status(404).send({msg: 'Path not found'})
     })
 
+app.use(handlePSQLErrors)
 
-    app.use(handlePSQLErrors)
-
-    app.use(handleCustomErrors)
+app.use(handleCustomErrors)
 
     module.exports = app
